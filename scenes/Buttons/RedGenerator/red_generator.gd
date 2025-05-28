ec1dae27c42ab1c57292805991d4d1df5d908de3
MasterMindGD <leongamedesigner@gmail.com>
2025-05-28 16:25:42 -0300

    Función generate_block creada y anexada a ambos scripts

--- scenes/Buttons/RedGenerator/red_generator.gd
+++ scenes/Buttons/RedGenerator/red_generator.gd
@@ -1,5 +1,11 @@
 extends TextureButton
-
+#Llama a la Scena para poder instanciarla
+var RedBlock = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
 func _on_button_down() -> void:
-	pass # Replace with function body.
+	generate_block()
+#Función para generar los bloques
+func generate_block():
+	var instance = RedBlock.instantiate()
+	get_parent().add_child(instance) #Se hace hija del RedGenerator
+	instance.position = self.position #Para coordinar con las coordenadas del padre
--- scenes/Buttons/VioletGenerator/violet_generator.gd
+++ scenes/Buttons/VioletGenerator/violet_generator.gd
@@ -1,5 +1,11 @@
 extends TextureButton
-
+#Llama a la Scena para poder instanciarla
+var VioletBlock = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
 
 func _on_button_down() -> void:
-	pass # Replace with function body.
+	generate_block()
+#Función para generar los bloques
+func generate_block():
+	var instance = VioletBlock.instantiate() 
+	get_parent().add_child(instance) #Se hace hija del VioletGenerator
+	instance.position = self.position #Para coordinar con las coordenadas del padre
